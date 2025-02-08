e92f225e9aceebb9afba3b3830022dafe428c524
NSG18 <gangurdenakul@gmail.com>
2025-02-08 23:00:14 +0530

    fix dynamic post problem attemp-4

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -25,8 +25,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
-    const { id } = context.params;
+export async function GET(req: NextRequest) {
+    const url = new URL(req.url);
+    const id = url.pathname.split("/").pop();
 
     if (!id) {
         return NextResponse.json({ error: "ID is required" }, { status: 400 });
