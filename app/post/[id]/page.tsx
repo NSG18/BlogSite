09437dabdd131dbc9288d4bcb09ea8e649b3a465
NSG18 <gangurdenakul@gmail.com>
2025-02-13 20:14:13 +0530

    id

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,34 +1,3 @@
-// import PostSus from "@/app/Components/postSus";
-// import prisma from "@/lib/prisma"
-// import { notFound } from "next/navigation";
-// import { Suspense } from "react";
-
-
-// type tParams = Promise<{ slug: string[] }>;
-
-
-// export default async function Page({ params }: { params: tParams }) {
-//   const { slug }: { slug: string[] } = await params;
-//   const id = slug[0];
-
-//   const post = await prisma.post.findUnique({
-//     where: {
-//       id: id,
-//     },
-//   });
-//   if (!post) {
-//     notFound();
-//   }
-
-//   return (
-//     <Suspense fallback={<PostSus />}>
-//       <main className="min-h-screen px-7 pt-24 text-center">
-//         <h1 className="text-5xl font-semibold mb-7">{post.title}</h1>
-//         <p className="max-w-[700px] mx-auto">{post.content}</p>
-//       </main>
-//     </Suspense>
-//   );
-// }
 
 
 import PostSus from "@/app/Components/postSus";
